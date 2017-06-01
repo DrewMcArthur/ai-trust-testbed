589874341bb9fa37a46c13e50bdda433bc15f831
DrewMcArthur <drewmcarthur1@gmail.com>
2017-06-01 12:12:08 -0400

    changed config file to YAML, began compile_data script

---
+++ compile_data.py
@@ -0,0 +1,9 @@
+"""
+ *  Drew McArthur, Geo Engel, Risa Ulinski, Judy Zhou
+ *  6/1/17
+ *  a script to produce a single datafile from the race and horse information 
+    located in the raw data folder (defined in config) for use with an AI
+"""
+
+import yaml
+config = yaml.safe_load(open("config.yml"))
