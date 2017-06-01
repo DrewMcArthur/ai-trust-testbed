6888718065a5519e4e29df5a2749a69043856859
DrewMcArthur <drewmcarthur1@gmail.com>
2017-06-01 15:46:54 -0400

    added small comments to compile_data

--- compile_data.py
+++ compile_data.py
@@ -3,7 +3,11 @@
  *  6/1/17
  *  a script to produce a single datafile from the race and horse information 
     located in the raw data folder (defined in config) for use with an AI
+ *  note: use python3
 """
 
 import yaml
 config = yaml.safe_load(open("config.yml"))
+
+# right way to access config vars
+# config['raw_data_path']
