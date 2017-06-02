2c43e4bad4f96d3803c3bc8f313967d4e4a804b2
DrewMcArthur <drewmcarthur1@gmail.com>
2017-06-02 10:20:24 -0400

    initial reading of csv data

--- compile_data.py
+++ compile_data.py
@@ -6,8 +6,25 @@
  *  note: use python3
 """
 
-import yaml
+import yaml, os, csv
 config = yaml.safe_load(open("config.yml"))
 
 # right way to access config vars
 # config['raw_data_path']
+
+# get root folder, as well as pathname and file objects for the final product.
+DATA = config['raw_data_path']
+ENDFILENAME = config['final_data_filename']
+ENDFILE = open(ENDFILENAME, 'w')
+#ENDFILEWRITER = csv.DictWriter(ENDFILE, config['data_col_headers'])
+
+# iterate through files in data directory
+for d in os.listdir(DATA):
+    for f in os.listdir(DATA + d):
+        if f.endswith('.csv'):
+            path = DATA + "/" + d + "/" + f
+            with open(path, newline='') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    print(row)
+                    quit()
