a30b60a529a4d99cd0eef7c68e63aa6d49fb7bce
DrewMcArthur <drewmcarthur1@gmail.com>
2017-06-05 10:27:56 -0400

    now creates a file for race data and horse data, but includes empty lines

--- compile_data.py
+++ compile_data.py
@@ -19,27 +19,43 @@ config = yaml.safe_load(open("config.yml"))
 DATA = config['raw_data_path']
 ENDFILENAME = config['final_data_filename']
 ENDFILE = open(ENDFILENAME, 'w')
+RACEFILENAME = "RACES." + ENDFILENAME
+RACEFILE = open(RACEFILENAME, 'w')
+HORSEFILENAME = "HORSES." + ENDFILENAME
+HORSEFILE = open(HORSEFILENAME, 'w')
 
-headers = config['data_col_headers'].split(',')
-headers[-1] = headers[-1][:-1]
+raceHeaders = config['race_data_col_headers'].split(',')
+raceHeaders[-1] = raceHeaders[-1][:-1]
+horseHeaders = config['horse_data_col_headers'].split(',')
+horseHeaders[-1] = horseHeaders[-1][:-1]
 
 # create an object which writes data to files as a csv, using column headers 
 # from config.yml and ignoring extra data
-ENDFILEWRITER = csv.DictWriter(ENDFILE, fieldnames=headers, 
+#ENDFILEWRITER = csv.DictWriter(ENDFILE, fieldnames=headers, 
+#                               extrasaction='ignore', dialect='unix')
+RACEFILEWRITER = csv.DictWriter(RACEFILE, fieldnames=raceHeaders, 
+                               extrasaction='ignore', dialect='unix')
+HORSEFILEWRITER = csv.DictWriter(HORSEFILE, fieldnames=horseHeaders, 
                                extrasaction='ignore', dialect='unix')
 
 # if ENDFILE is empty, then write the header columns to the file.
-if os.stat(ENDFILENAME).st_size == 0:
-    ENDFILEWRITER.writeheader()
+#if os.stat(ENDFILENAME).st_size == 0:
+#    ENDFILEWRITER.writeheader()
+if os.stat(RACEFILENAME).st_size == 0:
+    RACEFILEWRITER.writeheader()
+if os.stat(HORSEFILENAME).st_size == 0:
+    HORSEFILEWRITER.writeheader()
 
 # iterate through files in data directory
 for d in os.listdir(DATA):
-    for f in os.listdir(DATA + d):
-        # if the file is a *.csv file,
-        if f.endswith('.csv'):
-            # then open the file with a csv reader
-            path = DATA + "/" + d + "/" + f
-            with open(path, newline='') as csvfile:
-                reader = csv.DictReader(csvfile, dialect='unix')
-                for row in reader:
-                    ENDFILEWRITER.writerow(row)
+    if os.path.isdir(DATA + d):
+        for f in os.listdir(DATA + d):
+            # if the file is a *.csv file,
+            if f.endswith('.csv'):
+                # then open the file with a csv reader
+                path = DATA + "/" + d + "/" + f
+                with open(path, newline='') as csvfile:
+                    reader = csv.DictReader(csvfile, dialect='unix')
+                    for row in reader:
+                        RACEFILEWRITER.writerow(row)
+                        HORSEFILEWRITER.writerow(row)
