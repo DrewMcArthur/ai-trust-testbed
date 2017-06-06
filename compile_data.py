d1030e5a2e3953ae4e9e29ce693f2294bb964820
DrewMcArthur <drewmcarthur1@gmail.com>
2017-06-06 15:29:24 -0400

    initial commit of writing label data, untested;

--- compile_data.py
+++ compile_data.py
@@ -7,9 +7,15 @@
     categorizing information for the races, horses, and labels,
     to then be used to compile the final datafile.
  *  note: use python3
+ *  TODO: read data from *{lt, lb}.csv files, write to LABELS.data.csv
+          create LABELWRITER object and appropriate associated files
+          sort data before writing to middle files
+            write sorting algorithm for a list of dictionaries that sorts by
+                certain keys
+            this way, reading from the middle files to merge them is easier
 """
 
-import yaml, os, csv
+import yaml, os, csv, re
 config = yaml.safe_load(open("config.yml"))
 
 # right way to access config vars
@@ -22,6 +28,7 @@ DATA = config['raw_data_path']
 ENDFILENAME = config['final_data_filename']
 RACEFILENAME = "RACES." + ENDFILENAME
 HORSEFILENAME = "HORSES." + ENDFILENAME
+LABELFILENAME = "LABELS." + ENDFILENAME
 
 def combineList(a, b):
     c = a[:]
@@ -35,6 +42,8 @@ raceHeaders = config['race_data_col_headers'].split(', ')
 raceHeaders[-1] = raceHeaders[-1][:-1]
 horseHeaders = config['horse_data_col_headers'].split(', ')
 horseHeaders[-1] = horseHeaders[-1][:-1]
+labelHeaders = config['label_data_col_headers'].split(', ')
+labelHeaders[-1] = labelHeaders[-1][:-1]
 headers = combineList(raceHeaders, horseHeaders)
 
 def rowEmpty(row, headers):
@@ -73,15 +82,58 @@ def writePreRaceInfo(f, folder, RACEWRITER, HORSEWRITER):
 def writeLabelInfo(f, folder, LABELWRITER):
     """ Scrapes data from file f in folder, and writes the data to 
         a labels file, using the object LABELWRITER """
+
     print('writing from',folder + "/" + f, 'to labelwriter')
-    path = folder + "/" _ f
     print("         ", f)
-    with open(path, newline='') as csvfile:
-        reader = csv.Dictreader(csvfile, dialect='unix')
-        raceIDInfo = {}
+
+    raceIDInfo = {"R_RCTrack": track, "R_RCDate": date, "R_RCRace": race}
+    print("dealing with this race: ")
+    print(raceIDInfo)
+
+    # list of dictionaries, each dict is a data entry to be written to LABELS
+    labeldata = []
+
+    # parse track and date from filename
+    m = re.match("([a-zA-Z]{2,3})([0-9]{6})_([0-9]).*", f)
+    track = m.group(1)
+    date = m.group(2)
+    race = m.group(3)
+
+    # generate pathnames for the desired files
+    beyerpath = folder + "/" + track + date + "_" + race + "_lb.csv"
+    timepath = folder + "/" + track + date + "_" + race + "_lt.csv"
+
+    # open files for reading and create respective csv.DictReader objects
+    with open(beyerpath, newline='') as beyerfile, \
+         open(timepath, newline='') as timefile:
+        beyerreader = csv.DictReader(beyerfile, dialect='unix')
+        timereader = csv.DictReader(timefile, dialect='unix')
+
+        # add the data in the beyer label file to the list, 
+        # and simultaneously add ID and rank info for the race to the row
+        rank = 1
+        for row in beyerreader:
+            row.update(raceIDInfo))
+            row.update({"rank": rank})
+            labeldata.append(row)
+            rank += 1
+
+        # add data from the timereader to the dictionaries in that list
+        for row in timereader:
+            for entry in labeldata:
+                if row['Horse'] == entry['Horse']
+                    entry.update(row)
+
+        print("is this label data right???")
+        print(labeldata)
+
         # TODO: write to LABELS.data.csv
         #       create LABELWRITER and correct file to pass to this function
         quit()
+
+    # write the entries in labeldata to file
+    for entry in labeldata:
+        LABELWRITER.writerow(entry)
 
 def create_middle_files():
     """ iterate through files in DATA directory and create 
@@ -90,6 +142,7 @@ def create_middle_files():
     ENDFILE = open(ENDFILENAME, 'w')
     HORSEFILE = open(HORSEFILENAME, 'w')
     RACEFILE = open(RACEFILENAME, 'w+')
+    LABELFILE = open(LABELFILENAME, 'w')
 
     # create an object which writes data to files as a csv, using column headers
     # from config.yml and ignoring extra data
@@ -99,6 +152,9 @@ def create_middle_files():
                                    extrasaction='ignore', dialect='unix')
     HORSEWRITER = csv.DictWriter(HORSEFILE, fieldnames=horseHeaders, 
                                    extrasaction='ignore', dialect='unix')
+    LABELWRITER = csv.DictWriter(LABELFILE, fieldnames=labelHeaders, 
+                                   extrasaction='ignore', dialect='unix')
+
 
     # if ENDFILE is empty, then write the header columns to the file.
     #if os.stat(ENDFILENAME).st_size == 0:
@@ -123,7 +179,10 @@ def create_middle_files():
                         if f.endswith('sf.csv') :
                             writePreRaceInfo(f, folder, RACEWRITER, HORSEWRITER)
                         # if the file contains labels for races
-                        elif f.endswith('lb.csv') or f.endswith('lt.csv'):
+                        # elif f.endswith('lb.csv') or f.endswith('lt.csv'):
+                        # only looking for one of the label files, to avoid dups
+                        # the other filename is generated in the function below.
+                        elif f.endswith('lt.csv'):
                             writeLabelInfo(f, folder, LABELWRITER)
 
 def generate_data(n_horse):
